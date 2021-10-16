890f94b920408f4b3b62299e4b6e593e3040e3f9
Allan Ramos <allangrds@gmail.com>
2021-10-16 14:12:08 -0300

    feat: second class of daniel study

--- src/components/Class/livro-daniel.js
+++ src/components/Class/livro-daniel.js
@@ -3,6 +3,10 @@ const classes: any[] = [
     title: 'De Jerusalém a Babilônia',
     url: 'https://www.youtube.com/watch?v=lOdGMNEk9Uk',
   },
+  {
+    title: 'Médiuns versus Profeta',
+    url: 'https://www.youtube.com/watch?v=OIsuz8P_iuU',
+  },
 ]
 
 export default classes
